7f3948733c5e0b34ed36c5004a5a841b9a214d16
Melissa <melissa.scmi@gmail.com>
2021-09-27 11:59:36 -0700

    add 2 madlibs; wip: begin styling
    Co-authored by: Ben Dunham <bendunhampdx@gmail.com>

--- Madlibs/Controllers/HomeController.cs
+++ Madlibs/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Madlibs.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Madlibs.Controllers
@@ -6,13 +7,52 @@ namespace Madlibs.Controllers
   {
     [Route("/")]
     public ActionResult Madlibs() { return View(); }
-      {
-      MadlibsVariable myMadlibsVariable = new MadlibsVariable();
-      myMadlibsVariable.PLACEHOLDER = placeholder;
-      return View(myMadlibsVariable);
-      }
 
     [Route("/form")]
     public ActionResult Form() { return View(); }
+
+    [Route("/form2")]
+    public ActionResult Form2() { return View(); }
+
+    [Route("/yourmadlib")]
+    public ActionResult YourMadlib(string spaceTeamName, string adverbOne, string pluralNounOne, string adverbTwo, string adjectiveOne, string pluralNounTwo, string adjectiveTwo, string verbOne, string nounFormVerb, string adjectiveThree, string pluralNounThree, string fakeDogCountry)
+    {
+      MadlibsVariable myMadlibsVariable = new MadlibsVariable();
+      myMadlibsVariable.SpaceTeamName = spaceTeamName;
+      myMadlibsVariable.AdverbOne = adverbOne;
+      myMadlibsVariable.PluralNounOne = pluralNounOne;
+      myMadlibsVariable.AdverbTwo = adverbTwo;
+      myMadlibsVariable.AdjectiveOne = adjectiveOne;
+      myMadlibsVariable.PluralNounTwo = pluralNounTwo;
+      myMadlibsVariable.AdjectiveTwo = adjectiveTwo;
+      myMadlibsVariable.VerbOne = verbOne;
+      myMadlibsVariable.NounFormVerb = nounFormVerb;
+      myMadlibsVariable.AdjectiveThree = adjectiveThree;
+      myMadlibsVariable.PluralNounThree = pluralNounThree;
+      myMadlibsVariable.FakeDogCountry = fakeDogCountry;
+      return View(myMadlibsVariable);
+    }
+
+    [Route("/yourmadlib2")]
+    public ActionResult YourMadlib2(string dayOfWeek, string name, string carModel, string weatherAdjective, string emotion, string nounOne, string nounTwo, string verbOne, string adjectiveOne, string nounThree, string verbTwo, string adverb, string adjectiveTwo, string ingVerb, string nounFour)
+    {
+      MadlibsVariable myMadlibsVariable = new MadlibsVariable();
+      myMadlibsVariable.DayOfWeek = dayOfWeek;
+      myMadlibsVariable.Name = name;
+      myMadlibsVariable.CarModel = carModel;
+      myMadlibsVariable.WeatherAdjective = weatherAdjective;
+      myMadlibsVariable.Emotion = emotion;
+      myMadlibsVariable.NounOne = nounOne;
+      myMadlibsVariable.NounTwo = nounTwo;
+      myMadlibsVariable.VerbOne = verbOne;
+      myMadlibsVariable.AdjectiveOne = adjectiveOne;
+      myMadlibsVariable.NounThree = nounThree;
+      myMadlibsVariable.VerbTwo = verbTwo;
+      myMadlibsVariable.Adverb = adverb;
+      myMadlibsVariable.AdjectiveTwo = adjectiveTwo;
+      myMadlibsVariable.IngVerb = ingVerb;
+      myMadlibsVariable.NounFour = nounFour;
+      return View(myMadlibsVariable);
+    }
   }
 }
--- Madlibs/Models/MadlibsVariable.cs
+++ Madlibs/Models/MadlibsVariable.cs
@@ -2,7 +2,29 @@ namespace Madlibs.Models
 {
   public class MadlibsVariable
   {
-    public string PLACEHOLDER { get; set; }
-
+    public string SpaceTeamName { get; set; }
+    public string AdverbOne { get; set; }
+    public string PluralNounOne { get; set; }
+    public string AdverbTwo { get; set; }
+    public string AdjectiveOne { get; set; }
+    public string PluralNounTwo { get; set; }
+    public string AdjectiveTwo { get; set; }
+    public string VerbOne { get; set; }
+    public string NounFormVerb { get; set; }
+    public string AdjectiveThree { get; set; }
+    public string PluralNounThree { get; set; }
+    public string FakeDogCountry { get; set; }
+    public string DayOfWeek { get; set;}
+    public string Name { get; set;}
+    public string CarModel { get; set; }
+    public string WeatherAdjective { get; set; }
+    public string Emotion { get; set; }
+    public string Adverb { get; set; }
+    public string IngVerb { get; set; }
+    public string NounFour { get; set; }
+    public string NounOne { get; set; }
+    public string NounTwo { get; set; }
+    public string NounThree { get; set; }
+    public string VerbTwo { get; set; }
   }
 }
